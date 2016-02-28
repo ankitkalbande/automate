5edc867ce302b7676f62664b53bbebb2b4cdae49
ankitkalbande <ankitkalbande@gmail.com>
2016-02-28 10:50:30 +0100

    kindle update

--- kindle/kindle.sh
+++ kindle/kindle.sh
@@ -1,9 +1,11 @@
 st2="http://www.esahity.com"
-curl $1 | grep pdf | grep -o -P '(?<=href=\").*(?=pdf)'  > $2
+curl $1 | grep pdf | grep -o -P '(?<=href=\").*(?=pdf)'  > tmp
 mkdir $2
-mv $2 $2/.
+mv tmp $2/.
 cd $2
 while read p; do
   wget $st2$p"pdf" 
-done <katha
+done <tmp
+
+
 
